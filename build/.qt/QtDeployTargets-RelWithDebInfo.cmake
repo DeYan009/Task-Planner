17ba6b33f3c18b4343fe87e12e7a424064b5c4d6
DeYan009 <124430267+DeYan009@users.noreply.github.com>
2025-07-15 22:01:31 +0200

    add build

---
+++ build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_TP_FILE E:/university/Task-Planner/build/RelWithDebInfo/TP.exe)
+set(__QT_DEPLOY_TARGET_TP_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TP_RUNTIME_DLLS E:/qt2/6.9.1/msvc2022_64/bin/Qt6Widgets.dll;E:/qt2/6.9.1/msvc2022_64/bin/Qt6Gui.dll;E:/qt2/6.9.1/msvc2022_64/bin/Qt6Core.dll)
